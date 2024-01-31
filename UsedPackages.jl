5800f81c9966c2e2c43377c3a9a6349470599e31
LucasSenandeLopez <lsenandelopez03@gmail.com>
2024-01-31 22:48:52 +0100

    Quantitative Macroeconomics Assignment 1

--- UsedPackages.jl
+++ UsedPackages.jl
@@ -1,4 +1,4 @@
 using Pkg
-used_packages = ["BenchmarkTools", "OrderedCollections", "Plots", "Distributions"]
+used_packages = ["BenchmarkTools", "OrderedCollections", "Plots", "Distributions", "DataFrames", "CSV"]
 Pkg.add(used_packages)
 Pkg.update()
